dbd820b783cf34c71c2093e2d06f09f6203711e5
alifeee <alifeeerenn@hotmail.com>
2023-12-05 13:41:00 +0000

    add debug (disable print)

--- scripts/day05.sh
+++ scripts/day05.sh
@@ -4,26 +4,29 @@
 
 # cat $1 | sed 's/seeds: //g' | sed '2, $s/^.*: *//g' | sed '2, $s/ /:/g' | sed '2, $s/.$/;/g' | awk ' /^$/ { print; } /./ { printf("%s", $0); } ' | awk ' /^$/ { print; } /./ { printf("%s", $0); } '
 
-cat $1 | sed 's/seeds: //g' | sed '2, $s/^.*: *//g' | sed '2, $s/ /:/g' | sed 's/\(.\)$/\1;/g' | awk ' /^$/ { print; } /./ { printf("%s", $0); } ' | awk ' /^$/ { print; } /./ { printf("%s", $0); } ' | sed 's/;$//g' | awk -F';' ' 
+cat $1 | sed 's/seeds: //g' | sed '2, $s/^.*: *//g' | sed '2, $s/ /:/g' | sed 's/\(.\)$/\1;/g' | awk ' /^$/ { print; } /./ { printf("%s", $0); } ' | awk ' /^$/ { print; } /./ { printf("%s", $0); } ' | sed 's/;$//g' | awk -F';' '
+BEGIN {
+    debug = 0;
+}
 NR == 1 {
-    print "# seeds"
-    print "> " $0;
+    if (debug) print "# seeds"
+    if (debug) print "> " $0;
     n_items = split($0, seeds, / /);
     for (i=1; i<=n_items; i++) {
         olditems[i] = seeds[i];
     }
 }
 NR > 1 {
-    print "\n> " $0
+    if (debug) print "\n> " $0
 
-    printf("olditems: ");
+    if (debug) printf("olditems: ");
     for (i=1; i<=n_items; i++) {
-        printf("%s, ", olditems[i]);
+        if (debug) printf("%s, ", olditems[i]);
     }
-    print "";
+    if (debug) print "";
 
     for (i=1; i<=NF; i++) {
-        print $i;
+        if (debug) print $i;
         split($i, map, /:/);
         # print " destination range start: " map[1];
         # print " source range start: " map[2];
@@ -32,21 +35,21 @@ NR > 1 {
         lower_from = map[2];
         upper_from = (map[2] + map[3] - 1);
         lower_to = map[1]
-        print " map from [" lower_from "," upper_from "] to [" lower_to "," (map[1] + map[3] - 1) "]"
+        if (debug) print " map from [" lower_from "," upper_from "] to [" lower_to "," (map[1] + map[3] - 1) "]"
 
         for (item in olditems) {
-            print "  " olditems[item] " in range?";
+            if (debug) print "  " olditems[item] " in range?";
             if (olditems[item] >= lower_from && olditems[item] <= upper_from) {
                 height = olditems[item] - lower_from;
-                print "   yes! height from lower: " height ". new: " lower_to + height;
+                if (debug) print "   yes! height from lower: " height ". new: " lower_to + height;
                 newitems[item] = lower_to + height;
-            } else print "   no :("
+            } else if (debug) print "   no :("
         }
     }
 
     for (i=1; i<=n_items; i++) {
-        if (newitems[i]) {print "  new " newitems[i]}
-        else print "  old " olditems[i];
+        if (newitems[i]) {if (debug) print "  new " newitems[i]}
+        else if (debug)  print "  old " olditems[i];
         olditems[i] = newitems[i] ? newitems[i] : olditems[i];
     }
     delete newitems;
