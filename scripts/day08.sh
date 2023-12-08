939c3470d366b75cc2b925b6ff3c6c9117220526
alifeee <alifeeerenn@hotmail.com>
2023-12-08 14:01:31 +0000

    day 8 attempt (runs for >40 minutes, not sure if completes or not?)

--- scripts/day08.sh
+++ scripts/day08.sh
@@ -2,15 +2,16 @@
 
 # https://adventofcode.com/2023/day/8
 
-cat $1 | awk ' /^$/ { print } /./ { printf "%s;", $0 }' | awk -F';' 'NR == 1 {
+cat $1 | awk ' /^$/ { print } /./ { printf "%s;", $0 }' | tee >(awk -F';' 'NR == 1 {
     split($1, directions, "");
 } NR == 2 {
     nextloc = "AAA";
     acc = 0;
     direcindex = 0;
     while (1) {
+        break
         if (nextloc == "ZZZ") {
-            break
+            next
         }
         acc += 1;
         direcindex += 1;
@@ -40,5 +41,60 @@ cat $1 | awk ' /^$/ { print } /./ { printf "%s;", $0 }' | awk -F';' 'NR == 1 {
 
         if (debug) print " choosing " nextloc;
     }
+} END {
     print "part 1: " acc;
-}'
+}' > /dev/tty) | awk 'BEGIN {
+    debug = 0;
+} NR == 1 {
+    split($1, directions, "");
+} NR == 2 {
+    patsplit($0, nextlocs, /(..A) = \(([A-Z]{3}), ([A-Z]{3})\)/);
+    for (l in nextlocs) {
+        nextlocs[l] = substr(nextlocs[l], 0, 3);
+    }
+    acc = 0;
+    direcindex = 0;
+    while (1) {
+        if (debug) print acc
+
+        eq = 1;
+        for (l in nextlocs) {
+            if (!match(nextlocs[l], /..Z/)) {
+                eq=0;
+                break;
+            }
+        } if (eq) {
+            break;
+        }
+
+        acc += 1;
+        direcindex += 1;
+        if (direcindex > length(directions)) {
+            direcindex -= length(directions);
+        }
+
+        for (l in nextlocs) {
+            regex = "("nextlocs[l]") = \\(([A-Z]{3}), ([A-Z]{3})\\)"
+            match($0, regex, AAA);
+
+            all = AAA[0];
+            current = AAA[1];
+            left = AAA[2];
+            right = AAA[3];
+            direction = directions[direcindex];
+
+            if (debug) printf "%s = (%s, %s) - next: %s\n", current, left, right, direction;
+
+            direction = directions[direcindex];
+            if (direction == "L") {
+                nextlocs[l] = left;
+            } else if (direction == "R") {
+                nextlocs[l] = right
+            }
+        }
+
+        if (acc % 100 == 0) {
+            printf "\r%s", acc;
+        }
+    }
+} END { print "part 2: " acc }'
